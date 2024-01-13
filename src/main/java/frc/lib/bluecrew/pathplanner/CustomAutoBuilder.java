f786d8a0ce5f8c1af45a9112820945f36db57b6a
Gentry House <gentry@housefam.org>
2024-01-13 13:18:03 -0500

    Finished initial CustomAutoBuilder, not tested

--- src/main/java/frc/lib/bluecrew/pathplanner/CustomAutoBuilder.java
+++ src/main/java/frc/lib/bluecrew/pathplanner/CustomAutoBuilder.java
@@ -1,8 +1,18 @@
 package frc.lib.bluecrew.pathplanner;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.AutoBuilderException;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,23 +20,73 @@ import java.util.stream.Stream;
 
 public class CustomAutoBuilder extends com.pathplanner.lib.auto.AutoBuilder {
 
+    public static SendableChooser<Command> buildAutoChooserFromAutosInPPFolder(String defaultAutoName, String pathplannerFolderName) {
+        if (!AutoBuilder.isConfigured()) {
+            throw new RuntimeException(
+                    "AutoBuilder was not configured before attempting to build an auto chooser");
+        }
+
+        SendableChooser<Command> chooser = new SendableChooser<>();
+        List<String> autoNames = getAllAutoNamesInFolder(pathplannerFolderName);
+
+        PathPlannerAuto defaultOption = null;
+        List<PathPlannerAuto> options = new ArrayList<>();
+
+        for (String autoName : autoNames) {
+            PathPlannerAuto auto = new PathPlannerAuto(autoName);
+
+            if (!defaultAutoName.isEmpty() && defaultAutoName.equals(autoName)) {
+                defaultOption = auto;
+            } else {
+                options.add(auto);
+            }
+        }
+
+        if (defaultOption == null) {
+            chooser.setDefaultOption("None", Commands.none());
+        } else {
+            chooser.setDefaultOption(defaultOption.getName(), defaultOption);
+        }
+
+        options.forEach(auto -> chooser.addOption(auto.getName(), auto));
+
+        return chooser;
+    }
+
     /**
      * Get a list of all auto names in the specified pathplanner folder
      *
      * @return List of all auto names
      */
-    public static List<String> getAllAutoNames(String pathplannerFolder) {
-        File[] autoFiles = new File(Filesystem.getDeployDirectory(), "pathplanner/autos").listFiles();
+    public static List<String> getAllAutoNamesInFolder(String pathplannerFolder) {
+        List<String> allAutoNames = AutoBuilder.getAllAutoNames();
+        List<String> wantedAutoNames = new ArrayList<>();
 
-        if (autoFiles == null) {
-            return new ArrayList<>();
+        for(String autoName : allAutoNames) {
+            try (BufferedReader br =
+                         new BufferedReader(
+                                 new FileReader(
+                                         new File(
+                                                 Filesystem.getDeployDirectory(), "pathplanner/autos/" + autoName + ".auto")))) {
+                StringBuilder fileContentBuilder = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    fileContentBuilder.append(line);
+                }
+
+                String fileContent = fileContentBuilder.toString();
+                JSONObject json = (JSONObject) new JSONParser().parse(fileContent);
+
+                if(json.get("folder") == pathplannerFolder) {
+                    wantedAutoNames.add(autoName);
+                }
+            } catch (AutoBuilderException e) {
+                throw e;
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
         }
 
-        return Stream.of(autoFiles)
-                .filter(file -> !file.isDirectory())
-                .map(File::getName)
-                .filter(name -> name.endsWith(".auto"))
-                .map(name -> name.substring(0, name.lastIndexOf(".")))
-                .collect(Collectors.toList());
+        return wantedAutoNames;
     }
 }
