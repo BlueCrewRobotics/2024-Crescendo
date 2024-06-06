2350fed03e4fa2a1199c4e152c7d65bfa5e1ee8f
LandonMorse <155490087+LandonMorse@users.noreply.github.com>
2024-06-06 17:26:21 -0400

    Update demopassCommandGroup.java

--- src/main/java/frc/robot/commands/demopassCommandGroup.java
+++ src/main/java/frc/robot/commands/demopassCommandGroup.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autos.AutoGetNote;
 import frc.robot.subsystems.noteplayer.NotePlayerSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveDrive;
 
@@ -9,6 +10,6 @@ public class demopassCommandGroup extends SequentialCommandGroup {
     public demopassCommandGroup(SwerveDrive swervDrive, NotePlayerSubsystem notePlayer) {
         // TODO: Add your sequential commands in the super() call, e.g.
         //           super(new OpenClawCommand(), new MoveArmCommand());
-        super(notePlayer.demothrow(), new demoturn(swervDrive), new FindAndGotoNote(swervDrive));
+        super(notePlayer.demothrow(), new demoturn(swervDrive), new AutoGetNote(swervDrive,notePlayer).andThen(notePlayer.intakeNote()));
     }
 }
